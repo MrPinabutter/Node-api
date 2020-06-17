ef1aadd0553cff1720962e3441a323b2fbce14ae
Vitor Rafael <vitorafael.r.c@gmail.com>
2020-06-17 04:55:21 -0300

    usando-handlebars

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-// Inicializando o express
 const express = require('express');
+const handlebars = require('express-handlebars');
+const Sequelize = require('sequelize').Sequelize
+
 
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/html/index.html')
-});
+// Conexão com o banco de dados
+const sequelize = new Sequelize('sistemadecadastro', 'root', 'r2d2c3poj', {
+    host:"localhost",
+    dialect: 'mysql'
+})
+// Configurando o template engine (Handlebars)
+app.engine('handlebars', handlebars({defaultLayout: 'main'})) // main => template padrão da aplicação
+app.set('view engine', 'handlebars')
 
-app.get
+// Rotas
+app.get('/cad', (req, res)=>{ 
+    res.render('formulario')    // Exibe o arquivo do formulario handlebars da pasta views 
+})
+
+
 
 // Ouvindo a porta 3001 do navegador
 app.listen(8081);
