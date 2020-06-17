ad56f93b23adf61183733edf506ab3a6bd627249
Vitor Rafael <vitorafael.r.c@gmail.com>
2020-06-16 23:32:59 -0300

    Gerando dados na tabela

--- mysql.js
+++ mysql.js
@@ -8,3 +8,39 @@ sequelize.authenticate()
     .then(()=>console.log("sucesso na conexão"))
     .catch((error)=>console.log("erro na conexão: " + error))
 
+const Postagem = sequelize.define('postagens', {
+    title:{
+        type: Sequelize.STRING
+    },
+    conteudo:{
+        type: Sequelize.TEXT
+    }
+})
+
+// PARA INSERIR DADOS NO BANCO
+Postagem.create({
+    title: "Primeiro post",
+    conteudo: "Utilizando o sequelize para enviar dados ao MySQL"
+})
+
+const Usuarios = sequelize.define('usuarios', {
+    nome:{
+        type: Sequelize.STRING
+    },
+    sobrenome:{
+        type: Sequelize.STRING
+    },
+    sexo:{
+        type: Sequelize.CHAR
+    },
+    idade:{
+        type: Sequelize.INTEGER
+    },
+    email:{
+        type: Sequelize.STRING
+    }
+})
+
+// CRIA AS TABELAS NO BANCO DE DADOS:
+// Usuarios.sync({force: true})    
+// Postagem.sync({force: true})    
