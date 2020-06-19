dfc0448463ea2c442d5ba7d34940a59cbb74f4e2
Vitor Rafael <vitorafael.r.c@gmail.com>
2020-06-19 03:49:40 -0300

    Metodo-POST

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-const Sequelize = require('sequelize').Sequelize
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize').Sequelize;
 
 
 const app = express();
@@ -14,11 +15,20 @@ const sequelize = new Sequelize('sistemadecadastro', 'root', 'r2d2c3poj', {
 app.engine('handlebars', handlebars({defaultLayout: 'main'})) // main => template padrão da aplicação
 app.set('view engine', 'handlebars')
 
+// Config Bodyparser
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+
 // Rotas
 app.get('/cad', (req, res)=>{ 
     res.render('formulario')    // Exibe o arquivo do formulario handlebars da pasta views 
 })
 
+app.post('/add', (req, res)=>{
+    res.send(`Titulo: ${req.body.titulo}, Conteúdo: ${req.body.conteudo}`)
+    res.body.conteudo
+})
 
 
 // Ouvindo a porta 3001 do navegador
