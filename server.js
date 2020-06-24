d50d9a90611048e80b0786e6ac1ec8eaad82b531
Vitor Rafael <vitorafael.r.c@gmail.com>
2020-06-23 22:42:13 -0300

    'ajustes'

--- server.js
+++ server.js
@@ -8,29 +8,35 @@ const app = express();
 
 
 // Configurando o template engine (Handlebars)
-app.engine('handlebars', handlebars({defaultLayout: 'main'})) // main => template padrão da aplicação
-app.set('view engine', 'handlebars')
+    app.engine('handlebars', handlebars({defaultLayout: 'main'})) // main => template padrão da aplicação
+    app.set('view engine', 'handlebars')
 
 // Config Bodyparser
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended:false}))
+    app.use(bodyParser.json())
 
 
 // Rotas
-app.get('/cad', (req, res)=>{ 
-    res.render('formulario')    // Exibe o arquivo do formulario handlebars da pasta views 
-})
-
-app.post('/add', (req, res)=>{
-    Post.create({
-        titulo: req.body.titulo,
-        conteudo: req.body.conteudo 
-    }).then(() => {
-        res.send("Post criado com sucesso!")
-    }).catch((erro) => {
-        res.send("Falha no envio do Post: " + erro)
+    app.get('/', (req, res) => {
+        Post.findAll().then((posts) => {
+            res.render('home', {posts: posts})
+        })
     })
-})
+
+    app.get('/cad', (req, res)=>{ 
+        res.render('formulario')    // Exibe o arquivo do formulario handlebars da pasta views 
+    })
+
+    app.post('/add', (req, res)=>{
+        Post.create({
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo 
+        }).then(() => {
+            res.redirect('/')
+        }).catch((erro) => {
+            res.send("Falha no envio do Post: " + erro)
+        })
+    })
 
 // Ouvindo a porta 3001 do navegador
 app.listen(8081);
